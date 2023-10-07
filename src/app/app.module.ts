4a393972d1913093f1fb069d0b853c2d978fd815
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2023-10-07 18:19:08 +0300

    deleted nereikalinga component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {LoginComponent} from './main/auth/login/login.component';
 import {ViewCommentComponent} from './main/view-comment/view-comment.component';
 
 @NgModule({
@@ -31,7 +30,6 @@ import {ViewCommentComponent} from './main/view-comment/view-comment.component';
     LinksComponent,
     FormComponent,
     RegisteredformComponent,
-    LoginComponent,
     ViewCommentComponent
   ],
   imports: [
@@ -46,7 +44,6 @@ import {ViewCommentComponent} from './main/view-comment/view-comment.component';
     ReactiveFormsModule,
     NgFor,
     RouterModule.forRoot([
-      {path: 'login', component: LoginComponent},
       {path: 'home', component: FormComponent},
       {path: 'full-form', component: RegisteredformComponent},
     ]),
