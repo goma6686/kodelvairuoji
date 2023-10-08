c3ec2e7cfcb891a33e379a1a6faf7822a088cc2c
Foteno <72500101+Foteno@users.noreply.github.com>
2023-10-08 11:33:21 +0300

    add input for when loggedin

--- src/app/main/form/form.component.ts
+++ src/app/main/form/form.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {CommentService} from "../../services/comment.service";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-form',
@@ -11,16 +12,22 @@ export class FormComponent implements OnInit {
 
   comments = new FormControl('');
   carNumber: string = "";
-  message: string = "";
+  presetText: string = "";
+  text: string = "";
   presetComments: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
-  constructor(private commentService: CommentService) {
+  constructor(private commentService: CommentService,
+              public userService: UserService) {
   }
 
   ngOnInit(): void {
   }
 
   addComment() {
-    this.commentService.addComment(this.carNumber, this.message);
+    if (this.userService.isLoggedIn.getValue()) {
+      this.commentService.addComment(this.carNumber, this.text !== "" ? this.text : this.presetText);
+    } else {
+      this.commentService.addComment(this.carNumber, this.presetText);
+    }
   }
 }
